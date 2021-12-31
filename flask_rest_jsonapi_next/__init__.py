ed8616f4f26d18b41536e2ffe376d1dacde73657
Tomislav Adamic <tomislav.adamic@gmail.com>
2021-12-31 12:09:59 +0100

    build: move build and tools config to pyproject.toml
    - bump2version finally no longer overwrites settings for other tools

--- flask_rest_jsonapi_next/__init__.py
+++ flask_rest_jsonapi_next/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+__version__ = "0.31.2"
+
 from flask_rest_jsonapi_next.api import Api
 from flask_rest_jsonapi_next.resource import ResourceList, ResourceDetail, ResourceRelationship
 from flask_rest_jsonapi_next.exceptions import JsonApiException
